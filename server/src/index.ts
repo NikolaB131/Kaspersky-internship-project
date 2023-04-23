59eeb5e58b5b7e8b4b40a1548cbd9d7e04f5962e
Nikolai Balantsev <Nikolai.Balantsev@yandex.ru>
2023-04-23 13:53:45 +0300

    Add server

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import mockData from '../mock_data.json';
+
+const app = express();
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+  console.log('\x1b[36m%s\x1b[0m', `http://localhost:${port}`);
+});
+
+app.get('/api/users', (req, res) => {
+  const timeout = Math.floor(Math.random() * 400);
+  setTimeout(() => {
+    res.json(mockData);
+  }, timeout)
+});
